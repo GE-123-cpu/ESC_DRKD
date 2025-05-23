cd72fda8d681ba1d79be274f3384d5b6eb4ad1fc
Chenkun_Ge <73508872+GE-123-cpu@users.noreply.github.com>
2025-05-23 12:47:12 +0800

    Add files via upload

---
+++ MRI/data_MRI.py
@@ -0,0 +1,80 @@
+from PIL import Image
+from torchvision import transforms
+import os
+from torch.utils.data import Dataset, DataLoader
+import cv2
+
+
+transform = transforms.Compose([
+    transforms.Resize([256, 256]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+])
+
+transform1 = transforms.Compose([
+    transforms.Resize([256, 256]),
+    transforms.ToTensor()
+])
+
+
+
+class trainFolder(Dataset):
+    def __init__(self, image_dir):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.image_list = os.listdir(image_dir)
+
+
+    def __getitem__(self, index):
+        image_path = os.path.join(self.image_dir, self.image_list[index])
+        inputs = Image.open(image_path).convert('RGB')
+        images = self.transform(inputs)
+        img = cv2.imread(image_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img / 255., (256, 256))
+        return images, img
+
+    def __len__(self):
+        return len(self.image_list)
+
+
+class ImageDataset(Dataset):
+    def __init__(self, normal_dir, anomalous_dir1, anomalous_dir2, anomalous_dir3):
+        self.normal_dir = normal_dir
+        self.anomalous_dir1 = anomalous_dir1
+        self.anomalous_dir2 = anomalous_dir2
+        self.anomalous_dir3 = anomalous_dir3
+        self.transform = transform
+        self.normal_images = os.listdir(normal_dir)
+        self.anomalous_images1 = os.listdir(anomalous_dir1)
+        self.anomalous_images2 = os.listdir(anomalous_dir2)
+        self.anomalous_images3 = os.listdir(anomalous_dir3)
+
+    def __len__(self):
+        return len(self.normal_images) + len(self.anomalous_images1) + len(self.anomalous_images2) + len(self.anomalous_images3)
+
+    def __getitem__(self, index):
+        if index < len(self.normal_images):  # 加载正常图像
+            image = Image.open(os.path.join(self.normal_dir, self.normal_images[index])).convert('RGB')
+            label = 0
+        else:  # 加载异常图像
+            index -= len(self.normal_images)
+            if index < len(self.anomalous_images1):  # 异常路径1
+                image = Image.open(
+                    os.path.join(self.anomalous_dir1, self.anomalous_images1[index])).convert('RGB')
+                label = 1
+            elif index < len(self.anomalous_images1) + len(self.anomalous_images2):  # 异常路径2
+                index -= len(self.anomalous_images1)
+                image = Image.open(
+                    os.path.join(self.anomalous_dir2, self.anomalous_images2[index])).convert('RGB')
+                label = 1
+            else:  # 异常路径3
+                index -= len(self.anomalous_images1) + len(self.anomalous_images2)
+                image = Image.open(
+                    os.path.join(self.anomalous_dir3, self.anomalous_images3[index])).convert('RGB')
+                label = 1
+
+        if self.transform:
+            image = self.transform(image)  # 返回图像及其标签
+        return image, label  # 定义图像预处理
---
+++ MRI/train_MRI.py
@@ -0,0 +1,185 @@
+import argparse
+import time
+from tqdm import tqdm
+from data_MRI import *
+from fun import *
+from utils import time_file_str, time_string, convert_secs2time, AverageMeter, print_log, EarlyStop
+import numpy as np
+from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.metrics import roc_curve
+from scipy.ndimage import gaussian_filter
+import random
+from resnet import resnet18, resnet34, resnet50, wide_resnet50_2
+from de_resnet import de_resnet18, de_resnet34, de_wide_resnet50_2, de_resnet50
+from model import MultiProjectionLayer
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='MRI anomaly detection')
+    parser.add_argument('--obj', type=str, default='MRI')
+    parser.add_argument('--data_type', type=str, default='MRI')
+    parser.add_argument('--train_data_path', type=str, default='./MRI/Training/notumor')
+    parser.add_argument('--test_normaldata_path', type=str, default='./MRI/Testing/notumor')
+    parser.add_argument('--ab1', type=str, default='./MRI/Testing/glioma')
+    parser.add_argument('--ab2', type=str, default='./MRI/Testing/meningioma')
+    parser.add_argument('--ab3', type=str, default='./MRI/Testing/pituitary')
+    parser.add_argument('--proj_lr', default=0.001, type=float)
+    parser.add_argument('--distill_lr', default=0.005, type=float)
+    parser.add_argument('--epochs', type=int, default=50, help='maximum training epochs')
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--grayscale', action='store_true', help='color or grayscale input image')
+    parser.add_argument('--weight_decay', type=float, default=0.00001, help='decay of Adam')
+    parser.add_argument('--seed', type=int, default=None, help='manual seed')
+    args = parser.parse_args()
+
+    #args.input_channel = 1 if args.grayscale else 3
+
+    if args.seed is None:
+        args.seed = random.randint(1, 10000)
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+    if use_cuda:
+        torch.cuda.manual_seed_all(args.seed)
+
+    args.prefix = time_file_str()
+    args.save_dir = './' + args.data_type + '/' + args.obj + '/seed_{}/'.format(args.seed)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    log = open(os.path.join(args.save_dir, 'model_training_log_{}.txt'.format(args.prefix)), 'w')
+    state = {k: v for k, v in args._get_kwargs()}
+    print_log(state, log)
+
+    encoder, _ = wide_resnet50_2(pretrained=True)
+    encoder = encoder.to(device)
+    encoder.eval()
+    decoder = de_wide_resnet50_2(pretrained=False)
+    decoder = decoder.to(device)
+    proj_layer = MultiProjectionLayer(base=64).to(device)
+
+
+    optimizer_proj = torch.optim.Adam(list(proj_layer.parameters()), lr=args.proj_lr, betas=(0.5, 0.999))
+    optimizer_distill = torch.optim.Adam(list(decoder.parameters()), lr=args.distill_lr, betas=(0.5, 0.999))
+
+    kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
+
+    train_dataset = trainFolder(args.train_data_path)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_dataset = ImageDataset(args.test_normaldata_path, args.ab1, args.ab2, args.ab3)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True, **kwargs)
+
+    # start training
+    save_name = os.path.join(args.save_dir, '{}_{}_model.pth'.format(args.obj, args.prefix))
+    early_stop = EarlyStop(patience=20, save_name=save_name)
+    start_time = time.time()
+    epoch_time = AverageMeter()
+    for epoch in range(1, args.epochs + 1):
+        # adjust_learning_rate(args, optimizer, epoch)
+        need_hour, need_mins, need_secs = convert_secs2time(epoch_time.avg * (args.epochs - epoch))
+        need_time = '[Need: {:02d}:{:02d}:{:02d}]'.format(need_hour, need_mins, need_secs)
+        print_log(' {:3d}/{:3d} ----- [{:s}] {:s}'.format(epoch, args.epochs, time_string(), need_time), log)
+        train(proj_layer, decoder, epoch, train_loader, optimizer_proj, optimizer_distill, log, encoder)
+
+        scores, test_imgs, recon_imgs, gt_list, gt_mask_list, img_scores = test(decoder, proj_layer, test_loader, encoder)
+        gt_list = np.asarray(gt_list)
+        fpr, tpr, _ = roc_curve(gt_list, img_scores)
+        img_roc_auc = roc_auc_score(gt_list, img_scores)
+        ap = average_precision_score(gt_list, img_scores)
+        val_loss = - (img_roc_auc) * 100
+        print_log(('epoch: {} image: {:.4f} ap: {:.4f}'.format(epoch, img_roc_auc * 100, ap * 100)), log)
+        if (early_stop(val_loss, proj_layer, decoder, log)):
+            break
+
+        epoch_time.update(time.time() - start_time)
+        start_time = time.time()
+    log.close()
+
+
+def train(proj_layer, decoder, epoch, train_loader, optimizer_proj, optimizer_distill, log, encoder):
+    decoder.train()
+    proj_layer.train()
+
+    kd_losses = AverageMeter()
+    rec_losses = AverageMeter()
+    losses = AverageMeter()
+    cos = CosineLoss()
+
+    for (data, img) in tqdm(train_loader):
+        optimizer_proj.zero_grad()
+        optimizer_distill.zero_grad()
+        n, c, h, w = data.shape
+        #data = data.expand(n, 3, h, w)
+        large_value = random.randint(150, 200)
+        normal_value = random.randint(64, 100)
+        small_value = random.randint(10, 32)
+        weights = [0.33, 0.33, 0.33]
+        values = [large_value, normal_value, small_value]
+        t = random.choices(values, weights, k=1)[0]
+        img_noise_m = noise_generate(img, t)
+        data = data.to(device)
+        noise = img_noise_m.to(device)
+        input = encoder(data)
+        out_noise = encoder(noise)
+        output_noise = proj_layer(out_noise)
+        out = decoder(input[3], output_noise[2], output_noise[1])
+        rec_loss = cos(output_noise[1], input[1]) + cos(output_noise[2], input[2]) + cos(output_noise[0], input[0])
+        kd_loss = cos(out[0], input[0]) + cos(out[1], input[1]) + cos(out[2], input[2])
+        loss = rec_loss + kd_loss
+        rec_losses.update(rec_loss.item(), data.size(0))
+        kd_losses.update(kd_loss.item(), data.size(0))
+        losses.update(loss.item(), data.size(0))
+        loss.backward()
+        optimizer_proj.step()
+        optimizer_distill.step()
+
+    print_log(('Train Epoch: {} rec_Loss: {:.6f} kd_Loss: {:.6f} all_Loss: {:.6f}'.format(epoch, rec_losses.avg, kd_losses.avg, losses.avg)), log)
+
+def test(decoder, proj_layer, test_loader, encoder):
+    decoder.eval()
+    proj_layer.eval()
+    scores = []
+    test_imgs = []
+    gt_list = []
+    gt_mask_list = []
+    recon_imgs = []
+    image_scores = []
+    for (data, label) in tqdm(test_loader):
+        test_imgs.extend(data.cpu().numpy())
+        gt_list.extend(label.cpu().numpy().astype(int))
+        with torch.no_grad():
+            n, c, h, w = data.shape
+            data = data.expand(n, 3, h, w)
+            data = data.to(device)
+
+            inputs = encoder(data)
+            feature = proj_layer(inputs)
+            outputs = decoder(inputs[3], feature[2], feature[1])
+
+            anomaly_map1_kd = torch.ones(1, 64, 64).to(device) - F.cosine_similarity(outputs[0], inputs[0])
+            anomaly_map1_kd = anomaly_map1_kd.unsqueeze(0)
+            anomaly_map1_kd = F.interpolate(anomaly_map1_kd, size=(h, w), mode='bilinear', align_corners=True)
+            anomaly_map2_kd = torch.ones(1, 32, 32).to(device) - F.cosine_similarity(outputs[1], inputs[1])
+            anomaly_map2_kd = anomaly_map2_kd.unsqueeze(0)
+            anomaly_map2_kd = F.interpolate(anomaly_map2_kd, size=(h, w), mode='bilinear', align_corners=True)  # \
+            anomaly_map3_kd = torch.ones(1, 16, 16).to(device) - F.cosine_similarity(outputs[2], inputs[2])
+            anomaly_map3_kd = anomaly_map3_kd.unsqueeze(0)
+            anomaly_map3_kd = F.interpolate(anomaly_map3_kd, size=(h, w), mode='bilinear', align_corners=True)  # \
+            anomaly_map = (anomaly_map1_kd + anomaly_map2_kd + anomaly_map3_kd) / 3
+
+
+        score = anomaly_map.squeeze(0).cpu().numpy()
+        score = gaussian_filter(score, sigma=4)
+        s = np.max(score)
+        image_scores.append(s)
+        scores.append(score)
+        recon_imgs.extend(data.cpu().numpy())
+
+    return scores, test_imgs, recon_imgs, gt_list, gt_mask_list, image_scores
+
+
+if __name__ == '__main__':
+    main()
